1ef90bc301705762fa2f141cbadfbea7bf4d1a39
Brittany Kimbrough <bkimbrough87@gmail.com>
2015-07-01 10:48:30 -0500

    scroll

--- SpaceGame/MainMenuScene.swift
+++ SpaceGame/MainMenuScene.swift
@@ -14,6 +14,9 @@ class MainMenuScene: SKScene
 {
     var titleLabel = SKLabelNode(fontNamed: "Thonburi-Light")
     var tapScreenLabel = SKLabelNode(fontNamed: "SpaceOne")
+    
+    var backgroundOne : SKSpriteNode?
+    var backgroundTwo : SKSpriteNode?
 
 
     required init?(coder aDecoder: NSCoder)
@@ -27,9 +30,33 @@ class MainMenuScene: SKScene
         super.init(size: size)
 
         backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-
+        
+        addBackground()
+        
+        addBackgroundTwo()
+        
         addMainMenuSceneLabels()
     }
+    
+    func addBackground()
+    {
+        backgroundOne = SKSpriteNode(imageNamed: "sparsespace")
+        backgroundOne!.anchorPoint = CGPoint(x: 0.5, y: 0)
+        backgroundOne!.position = CGPoint(x: self.size.width / 2.0, y: 0)
+        
+        addChild(backgroundOne!)
+    }
+    
+    
+    func addBackgroundTwo()
+    {
+        backgroundTwo = SKSpriteNode(imageNamed: "sparsespace")
+        backgroundTwo!.anchorPoint = CGPoint(x: 0.5, y: 0)
+        backgroundTwo!.position = CGPoint(x: self.size.width / 4.0, y: backgroundOne!.position.y + backgroundOne!.size.height)
+        
+        addChild(backgroundTwo!)
+    }
+
 
 
     func addMainMenuSceneLabels()
@@ -57,6 +84,20 @@ class MainMenuScene: SKScene
         view?.presentScene(gameScene, transition: transition)
     }
     
-
-
+    override func update(currentTime: NSTimeInterval)
+    {
+        backgroundOne!.position = CGPointMake(self.size.width / 2, backgroundOne!.position.y - 6)
+        
+        backgroundTwo!.position = CGPointMake(self.size.width / 2, backgroundTwo!.position.y - 6)
+        
+        if backgroundOne!.position.y < -800 //-backgroundOne!.size.height
+        {
+            backgroundOne!.position = CGPointMake(self.size.width / 2, backgroundTwo!.position.y + 800)
+        }
+        
+        if backgroundTwo!.position.y < -800 //-backgroundTwo!.size.height
+        {
+            backgroundTwo!.position = CGPointMake(self.size.width / 2, backgroundOne!.position.y + 800)
+        }
+    }
 }
